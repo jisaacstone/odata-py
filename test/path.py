72b4a67818c321058f237e9b38f58f1192a43450
jisaacstone <jisaacstone@gmail.com>
2014-02-12 21:26:43 -0800

    more tests for path

--- test/path.py
+++ test/path.py
@@ -1,6 +1,7 @@
-from itertools import starmap
+from itertools import product, chain
 from sqlalchemy import select
 from odata import parser, urlpath
+from odata.exc import NotFoundException, RequestParseError
 from fixtures import tables, users, addresses
 import pytest
 
@@ -21,9 +22,52 @@ expected = [
     ]
 
 
+def make_context(verb):
+    return dict(sqlobj=parser.verbfuncs[verb],
+                response_headers={},
+                tables=tables,
+                request_payload=None)
+
+
 @pytest.mark.parametrize('verb,path,query', expected)
 def test_expected(verb, path, query):
-    context = dict(sqlobj=parser.verbfuncs[verb], headers={}, tables=tables,
-                   request_payload=None)
-    result = urlpath.parse(path, context)
+    context = make_context(verb)
+    urlpath.parse(path, context)
     assert unicode(context['sqlobj']) == query
+
+
+def test_value():
+    context = make_context('GET')
+    path = '/users/name/$value'
+    urlpath.parse(path, context)
+    assert unicode(context['sqlobj']) == unicode(
+        select().select_from(users).column(users.c.name))
+    assert 'Content-Type' in context['response_headers']
+    assert context['response_headers']['Content-Type'] == 'text/plain'
+
+
+raise404urls = ['/nope', '/users/nope']
+raise404 = product(parser.verbfuncs.keys(), raise404urls)
+
+
+@pytest.mark.parametrize('verb,path', raise404)
+def test_raise404(verb, path):
+    context = make_context(verb)
+    with pytest.raises(NotFoundException):
+        urlpath.parse(path, context)
+
+
+raiserpe = chain(
+    product(['POST', 'PUT', 'PATCH', 'MERGE', 'DELETE'],
+            ['/users/id/$value', 'users/$count']),
+    [('GET', '/users/$value'),
+     ('GET', '/$value'),
+     ('GET', '/$count'),
+     ])
+
+
+@pytest.mark.parametrize('verb,path', raiserpe)
+def test_raiserpe(verb, path):
+    context = make_context(verb)
+    with pytest.raises(RequestParseError):
+        urlpath.parse(path, context)
